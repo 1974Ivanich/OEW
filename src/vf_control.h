bd2db9c67739da233fcf424aae39c726f1b286db
1974Ivanich <122426047+1974Ivanich@users.noreply.github.com>
2026-08-31 12:54:13 +0400

    vf: keep header formatting unchanged

--- src/vf_control.h
+++ src/vf_control.h
@@ -9,7 +9,7 @@
 #define VFC_START_OK                    0
 #define VFC_START_ALREADY_RUNNING      -1
 #define VFC_START_FOC_ACTIVE           -2
-#define VFC_START_FAULT_LATCHED       -3
+#define VFC_START_FAULT_LATCHED        -3
 #define VFC_START_CONTEXT_UNVERIFIED   -4
 #define VFC_START_SELECTOR_FAILED      -5
 #define VFC_START_VECTOR_FAILED        -6
