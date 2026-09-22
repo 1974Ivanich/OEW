6f1683c255c103d275947d22d3f70b0eba1fd0c8
1974Ivanich <122426047+1974Ivanich@users.noreply.github.com>
2026-09-22 13:43:32 +0400

    fix(cli): correct rpm command line endings

--- src/cli.c
+++ src/cli.c
@@ -308,10 +308,8 @@ int CLI_ProcessLine(const char *line, const CLI_Ops *ops, CLI_State *state)
         else send_text(ops,"@PI:ERROR:NOT_CALCULATED\r\n> ");
     } else if (strcmp(line, "stats") == 0) { ops->autotune_print_stats(); send_text(ops, "> ");
     } else if (sscanf(line, "rpm=%d", &a1) == 1) {
-        if (a1 < -5000 || a1 > 5000) ops->send_text("err: rpm range -5000..+5000\\r\
-> ");
-        else { ops->foc_set_speed(a1); ops->send_telem("@RPM:OK:rpm=%ld\\r\
-> ", (long)a1); }
+        if (a1 < -5000 || a1 > 5000) send_text(ops, "err: rpm range -5000..+5000\r\n> ");
+        else { ops->foc_set_speed(a1); ops->send_telem("@RPM:OK:rpm=%ld\r\n> ", (long)a1); }
     } else if (sscanf(line, "i=%d,%d", &a1, &a2) == 2) { ops->foc_set_current(a1,a2); ops->send_telem("@I:OK:Id=%ld:Iq=%ld\\r\
 > ",(long)a1,(long)a2);
     } else if (strncmp(line, "run=", 4) == 0) {
